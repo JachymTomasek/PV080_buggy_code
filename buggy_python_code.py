0e02e159070ede4eb2b50e46eb441c54765db704
JachymTomasek <jachym@tomasek.work>
2026-05-05 12:45:12 +0200

    Fixes some errors.

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,4 @@
-import sys 
-import os
+import sys
 import yaml
 import flask
 
@@ -54,14 +53,14 @@ if __name__ == '__main__':
     choice  = input("Select vulnerability: ")
     if choice == "1": 
         new_person = Person("Vickie")  
-        print_nametag(input("Please format your nametag: "), new_person)
+        print_nametag(raw_input("Please format your nametag: "), new_person)
     elif choice == "2":
-        urlib_version = input("Choose version of urllib: ")
+        urlib_version = raw_input("Choose version of urllib: ")
         fetch_website(urlib_version, url="https://www.google.com")
     elif choice == "3":
-        load_yaml(input("File name: "))
+        load_yaml(raw_input("File name: "))
         print("Executed -ls on current folder")
     elif choice == "4":
-        password = input("Enter master password: ")
+        password = raw_input("Enter master password: ")
         authenticate(password)
 
