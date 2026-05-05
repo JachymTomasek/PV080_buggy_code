8a83f2377bdfcff66b85d956bf86c26891c6db2d
JachymTomasek <jachym@tomasek.work>
2026-05-05 12:51:32 +0200

    Fixes some errors.

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -44,6 +44,7 @@ def authenticate(password):
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
 
+
 if __name__ == '__main__':
     print("Vulnerabilities:")
     print("1. Format string vulnerability:")
@@ -51,16 +52,15 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
+    if choice == "1":
         new_person = Person("Vickie")  
-        print_nametag(raw_input("Please format your nametag: "), new_person)
+        print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
-        urlib_version = raw_input("Choose version of urllib: ")
+        urlib_version = input("Choose version of urllib: ")
         fetch_website(urlib_version, url="https://www.google.com")
     elif choice == "3":
-        load_yaml(raw_input("File name: "))
+        load_yaml(input("File name: "))
         print("Executed -ls on current folder")
     elif choice == "4":
-        password = raw_input("Enter master password: ")
+        password = input("Enter master password: ")
         authenticate(password)
-
